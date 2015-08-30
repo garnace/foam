5a020b056b451448d9278db1a97ea6bdafc1e078
garnace <farnace@rogers.com>
2015-08-29 23:16:49 -0400

    c

---
+++ public/gallery.php
@@ -0,0 +1,232 @@
+<?php 
+include ('./headerg.php');
+?>
+		
+		<!-- Page Wrapper -->
+		<!--div class="wrapper"-->
+		
+			<div class="banner padd">
+				<div class="container col-sm-4 col-md-4">
+					<!-- Image -->
+					<img class="img-responsive" src="img/crown-white.png" alt="" />
+					<!-- Heading -->
+					<h2 class="white">Gallery</h2>
+					<ol class="breadcrumb">
+						<li><a href="index.html">Home</a></li>
+						<li class="active">Gallery</li>
+					</ol>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+					<div class="clearfix"></div>
+			<!-- Header Start -->
+			
+			<div class="header">
+				<div class="container col-sm-12">
+					<!-- Header top area content -->
+					<!--div class="header-top"-->
+					<!--/div-->
+					<div class="row">
+						<div class="col-md-4 col-sm-5">
+							<!-- Link -->
+							<a href="index.html">
+								<!-- Logo area -->
+								<div class="logo">
+									<img class="img-responsive" src="img/logo.png" alt="" />
+									<!-- Heading -->
+									<h1>CakeFactory</h1>
+									<!-- Paragraph -->
+									<p>Facility ester expedite instinct.</p>
+<a id="fb3" class="btn btn-large btn-success" style="padding:15px 10 8px 10; padding-top:20;" onclick="getIniAniM(2);">Start</a>
+<a id="fb3" class="btn btn-large btn-success" style="padding:15px 10 8px 10; padding-top:20;" onclick="getEndAniM(2);">Reshuffle</a>
+<a id="fb3" class="btn btn-large btn-success" style="padding:15px 10 8px 10; padding-top:20;" onclick="getAniMPos(2);">Reshuffle</a>
+
+								</div>
+							</a>
+						</div>
+						<div class="col-md-8 col-sm-7">
+							<!-- Navigation -->
+							<nav class="navbar navbar-default navbar-right" role="navigation">
+								<div class="container-fluid">
+									<!-- Brand and toggle get grouped for better mobile display -->
+									<div class="navbar-header">
+										<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+											<span class="sr-only">Toggle navigation</span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+										</button>
+									</div>
+
+									<!-- Collect the nav links, forms, and other content for toggling -->
+									<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+										<ul class="nav navbar-nav">
+											<li><a href="index.html"><img src="img/nav-menu/nav1.jpg" class="img-responsive" alt="" /> Home</a></li>
+											<li class="dropdown hidden-xs">
+												<a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="img/nav-menu/nav2.jpg" class="img-responsive" alt="" /> Menu <b class="caret"></b></a>
+												<ul class="dropdown-menu dropdown-md">
+													<li>
+														<div class="row">
+															<div class="col-md-4 col-sm-6">
+																<!-- Menu Item -->
+																<div class="menu-item">
+																	<!-- Heading -->
+																	<h3>Vegetarian</h3>
+																	<!-- Image -->
+																	<img src="../cf/theme/img/dish/dish1.jpg" class="img-responsive" alt="" />
+																	<!-- Paragraph -->
+																	<p>Sea nut perspicacity under omni piste natures mirror of there with consequent.</p>
+																	<!-- Button -->
+																	<a href="menu.html" class="btn btn-danger btn-xs">View Menu</a>
+																</div>
+															</div>
+															<div class="col-md-4 col-sm-6">
+																<!-- Menu Item -->
+																<div class="menu-item">
+																	<!-- Heading -->
+																	<h3>Non-Vegetarian</h3>
+																	<!-- Image -->
+																	<img src="../cf/theme/img/dish/dish2.jpg" class="img-responsive" alt="" />
+																	<!-- Paragraph -->
+																	<p>Sea nut perspicacity under omni piste natures mirror as precode consequent.</p>
+																	<!-- Button -->
+																	<a href="menu.html" class="btn btn-danger btn-xs">View Menu</a>
+																</div>
+															</div>
+															<div class="col-md-4">
+																<!-- Menu Item -->
+																<div class="menu-item">
+																	<!-- Heading -->
+																	<h3>Special Menu</h3>
+																	<!-- Image -->
+																	<img src="../cf/theme/img/dish/dish3.jpg" class="img-responsive" alt="" />
+																	<!-- Paragraph -->
+																	<p>Sea nut perspicacity under omni piste natures mirror consequent.</p>
+																	<!-- Button -->
+																	<a href="menu.html" class="btn btn-danger btn-xs">View Menu</a>
+																</div>
+															</div>
+														</div>
+													</li>
+												</ul>
+											</li>
+											<li class="dropdown visible-xs">
+												<a href="#" class="dropdown-toggle" data-toggle="dropdown"> Menu <b class="caret"></b></a>
+												<ul class="dropdown-menu">
+													<li><a href="menu.html">Vegetarian</a></li>
+													<li><a href="menu.html">Non Vegetarian</a></li>
+													<li><a href="menu.html">Special Menu</a></li>
+												</ul>
+											</li>
+											<li><a href="gallery.html"><img src="img/nav-menu/nav3.jpg" class="img-responsive" alt="" /> Gallery</a></li>
+											<li class="dropdown">
+												<a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="img/nav-menu/nav4.jpg" class="img-responsive" alt="" /> Shop <b class="caret"></b></a>
+												<ul class="dropdown-menu">
+													<li><a href="items.html">Shopping</a></li>
+													<li><a href="item-single.html">Order Now</a></li>
+													<li><a href="checkout.html">Checkout</a></li>
+													<li><a href="reserve-seats.html">Reservation</a></li>
+													<li><a href="contact.html">Contact Us</a></li>
+												</ul>
+											</li>
+											<li class="dropdown">
+												<a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="img/nav-menu/nav5.jpg" class="img-responsive" alt="" /> Pages <b class="caret"></b></a>
+												<ul class="dropdown-menu">
+													<li><a href="error.html">404 Error</a></li>
+													<li><a href="0-base.html">Blank Page</a></li>
+													<li><a href="blog.html">Blog</a></li>
+													<li><a href="blog-single.html">Blog Single</a></li>
+													<li><a href="components.html">Components</a></li>
+													<li><a href="general.html">General</a></li>
+													<li><a href="nutrition-info.html">Nutrition Info</a></li>
+													<li><a href="recipe.html">Recipes</a></li>
+												</ul>
+											</li>
+											<li><a href="aboutus.html"><img src="img/nav-menu/nav6.jpg" class="img-responsive" alt="" /> About</a></li>
+										</ul>
+									</div><!-- /.navbar-collapse -->
+								</div><!-- /.container-fluid -->
+							</nav>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+//---------------------------------------------------------------
+
+				</div> <!-- / .container -->
+			</div>
+								<div class="clearfix"></div>
+			<!-- Header End -->
+			
+			<!-- Banner Start -->
+			
+			<div class="banner padd">
+				<div class="container col-sm-12">
+					<!-- Image -->
+					<img class="img-responsive" src="img/crown-white.png" alt="" />
+					<!-- Heading -->
+					<h2 class="white">Gallery</h2>
+					<ol class="breadcrumb">
+						<li><a href="index.html">Home</a></li>
+						<li class="active">Gallery</li>
+					</ol>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+								<div class="clearfix"></div>
+			<!-- Banner End -->
+			
+			
+			
+			<!-- Inner Content -->
+			<div class="inner-page padd">
+				
+				<!-- Gallery Start -->
+				
+				<div class="gallery">
+					<div class="container ">
+						<!-- Gallery elements with pretty photo -->
+						<div class="gallery-content board">
+						</div><!-- Separate gallery element --><!--/ End Gallery content class -->
+					</div>
+				</div>
+				
+				<!-- Gallery End -->
+			
+				<!-- Showcase Start -->
+				
+				<div class="showcase">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-6 col-sm-6">
+								<!-- Showcase section item -->
+								<div class="showcase-item">
+									<!-- Image -->
+									<img class="img-responsive" src="img/fruit2.png" alt="" />
+									<!-- Heading -->
+									<h3><a href="#">Equine Porno Sumos</a></h3>
+									<!-- Paragraph -->
+									<p>Nam libero tempore, cum soluta nobis est minis voluptas assum simple and easy to distinguis quo.</p>
+									<div class="clearfix"></div>
+								</div>
+							</div>
+							<div class="col-md-6 col-sm-6">
+								<!-- Showcase section item -->
+								<div class="showcase-item">
+									<!-- Image -->
+									<img class="img-responsive" src="img/fruit3.png" alt="" />
+									<!-- Heading -->
+									<h3><a href="#">Equine Porno Sumos</a></h3>
+									<!-- Paragraph -->
+									<p>Nam libero tempore, cum soluta nobis est minis voluptas assum simple and easy to distinguis quo.</p>
+									<div class="clearfix"></div>
+								</div>
+							</div><!-- /col-->
+						</div>
+					</div>
+				</div>
+				
+				<!-- Showcase End -->
+				
+			</div><!-- / Inner Page Content End -->	
+			
+<?php include ('./footerg.php')  ?>
