b91564e965335163bce2d568ae9fa9a91f89e7b7
garnace <root@darkstar.example.org>
2016-06-22 16:32:03 -0400

    upl18l

--- app.js
+++ app.js
@@ -109,8 +109,8 @@ app.use(function(req,res,next){
 app.get('/api/v1/recipes', recipesModel.getRecipes);
 app.get('/api/v1/recipesAll', recipesModel.getAllRecipes);
 app.get('/', serveFirstPage);
-//app.post('/upload',multiPartMiddleware,uploadFile);
-app.post('/upload',uploadFile);
+app.post('/upload',multiPartMiddleware,uploadFile);
+//app.post('/upload',uploadFile);
 //app.post('/upload',upStub);
 //app.post('/upload',upFile);
 app.get('/randomRecipe', serveRandomRecipe);
