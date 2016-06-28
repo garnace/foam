515c13cbf0f28d4f3ad242cce0e54de6f4e38628
garnace <root@darkstar.example.org>
2016-06-27 21:49:30 -0400

    upl23mn

--- app.js
+++ app.js
@@ -730,7 +730,7 @@ app.use(function(req, res, next) {
 
 
 
-app.use(function(err,req,res){
+app.use(function(err,req,res,next){
 	res.status(err.status || 500);
 	res.render('error',{
 		message: err.message,
